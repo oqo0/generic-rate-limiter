b6c2d90fdf2cea490573fff640822280fe09a309
oqo0 <oq.roman@gmail.com>
2024-07-05 16:23:05 +0300

    Refactor

--- src/GenericRateLimiter/Core/ActionRateLimiter.cs
+++ src/GenericRateLimiter/Core/ActionRateLimiter.cs
@@ -1,57 +1,38 @@
-namespace GenericRateLimiter.Core;
-
-/// <summary>
-/// Represents a rate limiter that limits the number of actions within a specified time period.
-/// </summary>
-public class ActionRateLimiter
+namespace GenericRateLimiter.Core
 {
-    public readonly long Limit;
-    public readonly TimeSpan Period;
-
-    private long _currentLimit;
-    private DateTime _onsetDateTime;
-    private readonly object _lock = new();
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="ActionRateLimiter"/> class.
-    /// </summary>
-    /// <param name="limit">The maximum number of allowed actions within the time period.</param>
-    /// <param name="period">The time period for rate limiting.</param>
-    public ActionRateLimiter(long limit, TimeSpan period)
+    public class ActionRateLimiter(long limit, TimeSpan period)
     {
-        Limit = limit;
-        Period = period;
-        _currentLimit = limit;
-        _onsetDateTime = DateTime.UtcNow;
-    }
+        public long Limit { get; } = limit;
+        public TimeSpan Period { get; } = period;
 
-    /// <summary>
-    /// Attempts to trigger the rate limiter without throwing an error.
-    /// </summary>
-    /// <returns>True if the action is rate limited, otherwise false.</returns>
-    public bool Trigger()
-    {
-        lock (_lock)
+        private long _currentLimit = limit;
+        private DateTime _lastResetTime = DateTime.UtcNow;
+
+        /// <summary>
+        /// Triggers the rate limiter.
+        /// </summary>
+        /// <returns>A boolean indicating whether the action is limited.</returns>
+        public bool Trigger()
         {
-            if (IsRateLimited())
-            {
-                return true;
-            }
+            ResetLimitIfPeriodElapsed();
 
+            if (_currentLimit <= 0)
+                return true;
+            
             _currentLimit--;
             return false;
         }
-    }
 
-    private bool IsRateLimited()
-    {
-        bool isExpired = DateTime.UtcNow - Period > _onsetDateTime;
-        if (isExpired)
+        /// <summary>
+        /// Resets the current limit if the period has elapsed.
+        /// </summary>
+        private void ResetLimitIfPeriodElapsed()
         {
+            if (DateTime.UtcNow - _lastResetTime <= Period)
+                return;
+            
             _currentLimit = Limit;
-            _onsetDateTime = DateTime.UtcNow;
+            _lastResetTime = DateTime.UtcNow;
         }
-
-        return _currentLimit <= 0;
     }
 }
