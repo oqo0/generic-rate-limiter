05a0f749dda8243e362a0063a1ced5e78bfbce5b
oqo0 <oq.roman@gmail.com>
2024-07-05 16:53:37 +0300

    Remove unused code

--- src/GenericRateLimiter.Sample/Program.cs
+++ src/GenericRateLimiter.Sample/Program.cs
@@ -14,28 +14,3 @@ builder.Services.AddRateLimiter<int>(x =>
 builder.Services.AddHostedService<SampleService>();
 
 await builder.Build().RunAsync();
-
-
-
-/*
-var rateLimiter = new EntityRateLimiter<int>(
-    new List<ActionRateLimiter>
-    {
-        new(1, TimeSpan.FromMilliseconds(100)),
-        new(3, TimeSpan.FromSeconds(1)),
-        new(30, TimeSpan.FromMinutes(1))
-    },
-    new WasteCleanerSettings(TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(10)));
-    */
-
-/*var rnd = new Random();
-long counter = 0;
-while (true)
-{
-    counter++;
-    var n = rnd.Next(int.MinValue, int.MaxValue);
-    var rateLimitStatus = rateLimiter.Trigger(n);
-
-    if (counter % 1_000_000 == 0)
-        Console.WriteLine(counter / 1_000_000);
-}*/
