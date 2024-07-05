0ddd10af3ec7557f19224c210667a4de14e8bef6
oqo0 <oq.roman@gmail.com>
2024-07-05 13:59:04 +0300

    Add sample project

---
+++ src/GenericRateLimiter.Sample/Program.cs
@@ -0,0 +1,41 @@
+﻿using GenericRateLimiter.Configuration;
+using GenericRateLimiter.Sample;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder();
+
+builder.Services.AddRateLimiter<int>(x =>
+{
+    x.AddRateLimiter(1, TimeSpan.FromSeconds(1));
+    x.AddRateLimiter(5, TimeSpan.FromSeconds(10));
+});
+
+builder.Services.AddHostedService<SampleService>();
+
+await builder.Build().RunAsync();
+
+
+
+/*
+var rateLimiter = new EntityRateLimiter<int>(
+    new List<ActionRateLimiter>
+    {
+        new(1, TimeSpan.FromMilliseconds(100)),
+        new(3, TimeSpan.FromSeconds(1)),
+        new(30, TimeSpan.FromMinutes(1))
+    },
+    new WasteCleanerSettings(TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(10)));
+    */
+
+/*var rnd = new Random();
+long counter = 0;
+while (true)
+{
+    counter++;
+    var n = rnd.Next(int.MinValue, int.MaxValue);
+    var rateLimitStatus = rateLimiter.Trigger(n);
+
+    if (counter % 1_000_000 == 0)
+        Console.WriteLine(counter / 1_000_000);
+}*/
---
+++ src/GenericRateLimiter.Sample/SampleService.cs
@@ -0,0 +1,33 @@
+﻿using GenericRateLimiter.Core;
+using Microsoft.Extensions.Hosting;
+
+namespace GenericRateLimiter.Sample;
+
+public class SampleService(IEntityRateLimiter<int> entityRateLimiter) : IHostedService
+{
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        while (true)
+        {
+            var keyChar = Console.ReadKey().KeyChar;
+            var rateLimitStatus = entityRateLimiter.Trigger(keyChar);
+
+            switch (rateLimitStatus)
+            {
+                case RateLimitStatus.Accessible:
+                    Console.WriteLine($" {DateTime.UtcNow} is Accessible");
+                    break;
+                case RateLimitStatus.Limited:
+                    Console.WriteLine($" {DateTime.UtcNow} is LIMITED");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return Task.CompletedTask;
+    }
+}
