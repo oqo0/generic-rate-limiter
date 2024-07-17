520eafd87fcd8a77249cef159d0fa519fd93e000
oqo0 <oq.roman@gmail.com>
2024-07-17 11:30:24 +0300

    Add waste limiter configurator

--- src/GenericRateLimiter/Configuration/Options/RateLimiterOptions.cs
+++ src/GenericRateLimiter/Configuration/Options/RateLimiterOptions.cs
@@ -1,11 +1,13 @@
 ﻿using GenericRateLimiter.Core;
 using GenericRateLimiter.Core.RateLimiters;
+using GenericRateLimiter.Core.WasteCleaners;
 
 namespace GenericRateLimiter.Configuration.Options;
 
 public class RateLimiterOptions()
 {
     private readonly List<ActionRateLimiter> _actionRateLimiters = [];
+    private WasteCleanerSettings _wasteCleanerSettings = GetDefaultWasteCleanerSettings();
 
     public RateLimiterOptions AddRateLimiter(int limit, TimeSpan period)
     {
@@ -13,8 +15,26 @@ public class RateLimiterOptions()
        return this;
     }
 
+    public RateLimiterOptions SetWasteCleanerSettings(WasteCleanerSettings wasteCleanerSettings)
+    {
+        _wasteCleanerSettings = wasteCleanerSettings;
+        return this;
+    }
+    
     public List<ActionRateLimiter> GetActionRateLimiters()
     {
         return _actionRateLimiters;
+    }
+    
+    public WasteCleanerSettings GetWasteCleanerSettings()
+    {
+        return _wasteCleanerSettings;
+    }
+    
+    private static WasteCleanerSettings GetDefaultWasteCleanerSettings()
+    {
+        return new WasteCleanerSettings(
+            TimeSpan.FromMinutes(1),
+            TimeSpan.FromMinutes(30));
     }
 }
